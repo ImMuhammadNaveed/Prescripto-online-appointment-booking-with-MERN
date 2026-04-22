d650f8fd90ecf8844380453ad39d54e497dd249e
Muhammad Naveed <itsnaveed277@gmail.com>
2026-04-22 22:51:46 +0500

    fix cookies in log in controller

--- backend/controllers/userContollers.js
+++ backend/controllers/userContollers.js
@@ -1,7 +1,7 @@
 const validator = require("validator")
 const bcrypt = require("bcrypt")
 const { userModel } = require("../database/usersModel")
-const {appointmentModel} = require("../database/appointmentModel")
+const { appointmentModel } = require("../database/appointmentModel")
 const { sign } = require("../middlewares/authentication")
 const { cloudinary } = require("../middlewares/cloudinary")
 const { doctorModel } = require("../database/doctorsModel")
@@ -49,7 +49,11 @@ async function login(req, res) {
             return res.status(400).json({ success: false, message: "Incorrect password!" })
         }
         const uToken = sign(user._id)
-        res.cookie("uToken", uToken)
+        res.cookie("uToken", uToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         return res.status(200).json({ success: true, message: "User successfully logged in!" })
     } catch (error) {
         return res.status(400).json({ success: false, message: error.message })
@@ -77,7 +81,7 @@ async function updateProfile(req, res) {
         console.log(req.body)
         imageFile = req.file
         let imageUrl
-        if(imageFile){
+        if (imageFile) {
             const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" })
             imageUrl = imageUpload.secure_url
         }
@@ -99,7 +103,7 @@ async function updateProfile(req, res) {
 async function allDoctors(req, res) {
     try {
         const doctors = await doctorModel.find({}).select("-password")
-        return res.status(200).json({ success: true, doctors: doctors})
+        return res.status(200).json({ success: true, doctors: doctors })
     } catch (error) {
         return res.status(400).json({ success: false, message: error.message })
     }
@@ -109,34 +113,34 @@ async function bookAppointment(req, res) {
     try {
         // console.log("book appointment controller hitted")
         const userId = req.userId
-        const {docId, slotDate, slotTime} = req.body
+        const { docId, slotDate, slotTime } = req.body
         // console.log(req.body)
         const docData = await doctorModel.findById(docId).select("-password")
-        if(!docData){
-            return res.status(500).json({success: false, message:"Doctor data not found!"})
+        if (!docData) {
+            return res.status(500).json({ success: false, message: "Doctor data not found!" })
         }
-        if(!docData.available){
-            return res.status(500).json({success: false, message:"Doctor not available!"})
+        if (!docData.available) {
+            return res.status(500).json({ success: false, message: "Doctor not available!" })
         }
         const userData = await userModel.findById(userId).select("-password")
         // console.log(userData)
-        if(!userData){
-            return res.status(500).json({success: false, message:"User not found!"})
+        if (!userData) {
+            return res.status(500).json({ success: false, message: "User not found!" })
         }
         //copying doctor booked slots
         let slotsBooked = docData.slots_booked
-        if(Array.isArray(slotsBooked)){
+        if (Array.isArray(slotsBooked)) {
             slotsBooked = {}
         }
         //checking if the doctor booked slots contain new booking date
-        if(slotsBooked[slotDate]){
+        if (slotsBooked[slotDate]) {
             //if contain than check if the new booking datae also contain new booking time
-            if(slotsBooked[slotDate].includes(slotTime)){
-                return res.status(500).json({success: false, message:"slot is already booked!"})
-            }else{
+            if (slotsBooked[slotDate].includes(slotTime)) {
+                return res.status(500).json({ success: false, message: "slot is already booked!" })
+            } else {
                 slotsBooked[slotDate].push(slotTime)
             }
-        }else{
+        } else {
             slotsBooked[slotDate] = []
             slotsBooked[slotDate].push(slotTime)
         }
@@ -155,56 +159,56 @@ async function bookAppointment(req, res) {
         console.log(appointment)
         const newAppointment = new appointmentModel(appointment)
         await newAppointment.save()
-        const updateDoctor = await doctorModel.findByIdAndUpdate(docId, {slots_booked: slotsBooked}, {new: true})
-        return res.status(200).json({success: true, message:"Slot booked!"})
+        const updateDoctor = await doctorModel.findByIdAndUpdate(docId, { slots_booked: slotsBooked }, { new: true })
+        return res.status(200).json({ success: true, message: "Slot booked!" })
     } catch (error) {
-        return res.status(500).json({success: false, message:error.message})
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
 async function myAppointments(req, res) {
     try {
         const userId = req.userId
-        const myAppointments = await appointmentModel.find({userId: userId})
-        return res.status(200).json({success: true, myAppointments: myAppointments})
+        const myAppointments = await appointmentModel.find({ userId: userId })
+        return res.status(200).json({ success: true, myAppointments: myAppointments })
     } catch (error) {
-        return res.status(500).json({success: false, message:error.message})
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
 async function cancelAppointment(req, res) {
     try {
-        const {appointmentId} = req.body
-        console.log("appointment id :",appointmentId)
-        const appointmentData = await appointmentModel.findByIdAndUpdate(appointmentId, {cancelAppointment: true})
-        if(!appointmentData){
-            return res.status(400).json({success: false, message:"appointment data not found"})
+        const { appointmentId } = req.body
+        console.log("appointment id :", appointmentId)
+        const appointmentData = await appointmentModel.findByIdAndUpdate(appointmentId, { cancelAppointment: true })
+        if (!appointmentData) {
+            return res.status(400).json({ success: false, message: "appointment data not found" })
         }
         const docId = appointmentData.doctorData._id
         console.log("doc id :", docId)
         const appointmentDate = appointmentData.slotDate
         const slotTime = appointmentData.slotTime
-        await doctorModel.findByIdAndUpdate(docId, {$pull:{[`slots_booked.${appointmentDate}`]: slotTime}})
-        return res.status(200).json({success: true, message:"appointment cancelled"})
+        await doctorModel.findByIdAndUpdate(docId, { $pull: { [`slots_booked.${appointmentDate}`]: slotTime } })
+        return res.status(200).json({ success: true, message: "appointment cancelled" })
     } catch (error) {
-        return res.status(500).json({success: false, message:error.message})
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
 async function logout(req, res) {
     try {
         res.clearCookie("uToken")
-        return res.status(200).json({success:true, message:"user successfully logged out!"})
+        return res.status(200).json({ success: true, message: "user successfully logged out!" })
     } catch (error) {
-        return res.status(500).json({success:false, message:error.message})
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
 async function isLoggedIn(req, res) {
     try {
-        return res.status(200).json({success: true, message:"admin is logged in!"})
+        return res.status(200).json({ success: true, message: "admin is logged in!" })
     } catch (error) {
-        return res.status(500).json({success: false, message:error.message})
+        return res.status(500).json({ success: false, message: error.message })
     }
 }
 
