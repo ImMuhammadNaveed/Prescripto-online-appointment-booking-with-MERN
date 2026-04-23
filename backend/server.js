557aef9e95d7444b517c3e993533413592a42dc1
Muhammad Naveed <itsnaveed277@gmail.com>
2026-04-23 17:36:24 +0500

    changing direct link in code

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const {doctorRouter} = require("./router/doctorRoutes")
 
 connectToDB()
 connectToCloudinary()
-const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5174"]
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL]
 app.use(cors({origin: allowedOrigins, credentials:true}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
