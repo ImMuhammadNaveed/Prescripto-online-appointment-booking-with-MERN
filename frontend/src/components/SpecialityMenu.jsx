bd7636f3129073d6d6ca47f2fa99e6aba79c40a2
Muhammad Naveed <itsnaveed277@gmail.com>
2026-05-07 09:31:15 +0500

    home page speciality menu responsiveness bug fixing

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -4,16 +4,16 @@ function SpecialityMenu() {
     const navigate = useNavigate()
     return (
         <>
-            <div className="mx-6 flex flex-col items-center justify-center py-10 mb-25" id="speciality-menu">
+            <div className="w-[92%] mx-auto overflow-hidden flex flex-col items-center justify-center py-10 mb-25" id="speciality-menu">
                 <p className="text-3xl pb-5 font-medium">Find by Speciality</p>
                 <p className=" text-sm mb-6 text-center">Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free</p>
-                <div className=" flex justify-start md:justify-center text-sm overflow-x-auto md:overflow-visible md:gap-4 gap-0 px-2">
+                <div className="w-full flex justify-start md:justify-center text-sm overflow-x-auto md:overflow-visible md:gap-4 gap-0 px-2">
                     {
                         specialityData.map((item, index) => (
                             <div onClick={() => navigate(`/doctors/${item.speciality}`)}
                                 key={index}
-                                className="flex flex-col items-center cursor-pointer min-w-[25%] md:min-w-0">
-                                <img src={item.image} alt="" className="md:w-20 w-20 object-contain" />
+                                className="flex flex-col items-center cursor-pointer min-w-[27%] md:min-w-0">
+                                <img src={item.image} alt="" className="md:w-20 w-16 object-contain" />
                                 <p className="mt-3 text-xs">{item.speciality}</p>
                             </div>
                         ))
